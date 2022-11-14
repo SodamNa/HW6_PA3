eed908fdb25c3c00b9ac10e0c423af48b2e23a13
SodamNa <78574152+SodamNa@users.noreply.github.com>
2022-11-13 19:43:09 -0800

    Update Q1.cpp

--- Q1.cpp
+++ Q1.cpp
@@ -11,18 +11,18 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        TreeNode* temp = root;
-        while(temp != NULL){
-            if(temp->val == val){
-                return temp;
+        TreeNode* temp = root; // Assign temp as root to search the val.
+        while(temp != NULL){ // Repeat searching until temp is empty.
+            if(temp->val == val){ // If a value of temp is the same as the val.
+                return temp; // Return temp and end. 
             }
-            else if(val < temp->val){
-                temp = temp->left;
+            else if(val < temp->val){ //If a value of temp is bigger than the val.
+                temp = temp->left; // Move to left child of the temp.
             }
-            else{
-                temp = temp->right;
+            else{ // If a value of temp is smaller than the val.
+                temp = temp->right; // Move to right child of the value.
             }
         }
-        return NULL;
+        return NULL; // End this function.
     }
 };
